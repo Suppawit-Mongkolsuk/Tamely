b3e389bf34fe02de35941a4d9cfe5902c7ad893a
Wisarut_folk <wisrutyaemprayur@gmail.com>
2026-03-04 06:28:38 +0700

    add-router

--- Frontend_mobile/app/index.tsx
+++ Frontend_mobile/app/index.tsx
@@ -1,10 +1,11 @@
-import "./global.css";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router"
 
-export default function App() {
-    return (
-        <View className="flex-1 justify-center items-center bg-white p-4">
-            <Text className="text-lg font-bold text-center text-gray-700">hello world</Text>
-        </View>
-    );
+export default function Index() {
+  const isLoggedIn = false
+
+  if (isLoggedIn) {
+    return <Redirect href="/(tabs)/feed" />
+  }
+
+  return <Redirect href="/(auth)/login" />
 }
