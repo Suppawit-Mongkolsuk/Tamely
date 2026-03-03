2659b3b1b78c1a57c21f312e8eaec3890bc04367
Wisarut_folk <wisrutyaemprayur@gmail.com>
2026-03-03 22:58:19 +0700

    finish_setup

--- Frontend_mobile/app/index.tsx
+++ Frontend_mobile/app/index.tsx
@@ -1,15 +1,10 @@
+import "./global.css";
 import { Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function App() {
+    return (
+        <View className="flex-1 justify-center items-center bg-white p-4">
+            <Text className="text-lg font-bold text-center text-gray-700">hello world</Text>
+        </View>
+    );
 }
